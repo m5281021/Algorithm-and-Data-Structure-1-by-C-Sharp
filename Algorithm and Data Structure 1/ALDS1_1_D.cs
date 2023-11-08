4fbbf8b178c8cd62b9519abc5803d97f32af29fb
s1290216 <s1290216@u-aizu.ac.jp>
2023-11-08 20:35:42 +0900

    adding an answer.

---
+++ Algorithm and Data Structure 1/ALDS1_1_D.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_and_Data_Structure_1
+{
+    class ALDS1_1_D
+    {
+        private int n = 0;
+        private List<int> list;
+
+        public ALDS1_1_D(int n, List<int> list)
+        {
+            this.n = n;
+            this.list = list;
+        }
+
+        public int MaximumProfit()
+        {
+            int max = -2000000000;
+            int min = list[0];
+            for(int i = 1; i < list.Count; i++)
+            {
+                max = Math.Max(max, list[i] - min);
+                min = Math.Min(min, list[i]);
+            }
+            return max;
+        }
+    }
+}
--- Algorithm and Data Structure 1/Program.cs
+++ Algorithm and Data Structure 1/Program.cs
@@ -5,12 +5,12 @@ internal class Program
     private static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine());
-        ALDS1_1_C alds1 = new ALDS1_1_C();
         List<int> list = new List<int>();
         for(int i = 0; i < n; i++)
         {
             list.Add(int.Parse(Console.ReadLine()));
         }
-        Console.WriteLine(alds1.PrimeNumbers(list));
+        ALDS1_1_D alds1 = new ALDS1_1_D(n, list);
+        Console.WriteLine(alds1.MaximumProfit());
     }
 }
