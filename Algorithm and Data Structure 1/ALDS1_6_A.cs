82d638162a8c54e7673aab23e106e30a6d40f937
s1290216 <s1290216@u-aizu.ac.jp>
2023-11-13 02:24:24 +0900

    adding the answer of the problem of counting sort.

---
+++ Algorithm and Data Structure 1/ALDS1_6_A.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_and_Data_Structure_1
+{
+    class ALDS1_6_A
+    {
+        private int[] list;
+        private int[] list2;
+        private int[] C = new int[10001];
+
+        public ALDS1_6_A(int[] list)
+        {
+            this.list = list;
+            list2 = new int[list.Length];
+        }
+
+        public void CountingSort()
+        {
+            int max = 0;
+            for(int i = 0; i < list.Length; i++)
+            {
+                max = Math.Max(max, list[i]);
+                C[list[i]]++;
+            }
+            for(int i = 1; i < max + 1; i++)
+            {
+                C[i] += C[i - 1];
+            }
+            for(int i = list.Length - 1; i >= 0; i--)
+            {
+                C[list[i]]--;
+                list2[C[list[i]]] = list[i];
+            }
+            Console.WriteLine(string.Join(" ", list2.Select(x => x.ToString()).ToArray()));
+        }
+    }
+}
--- Algorithm and Data Structure 1/Program.cs
+++ Algorithm and Data Structure 1/Program.cs
@@ -5,8 +5,8 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.ReadLine();
-        List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
-        ALDS1_5_D alds1 = new ALDS1_5_D(list);
-        alds1.Print();
+        int[] list = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        ALDS1_6_A alds1 = new ALDS1_6_A(list);
+        alds1.CountingSort();
     }
 }
